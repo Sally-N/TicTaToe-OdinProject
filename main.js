7c01b03c9441a4492a30d756cbfa2004aff448ea
Sally-N <snzungula@gmail.com>
2023-02-16 23:34:54 +0300

    design

--- main.js
+++ main.js
@@ -1,5 +1,39 @@
- const cellElements = document.querySelectorAll('.cell');
-console.log(cellElements);
+ const gameDetailsForm = document.getElementById("formone");
+//  console.log(gameDetailsForm);
 
-const echo = () => console.log("hi");
-cellElements.forEach(cellElement => {cellElement.addEventListener("click", echo, {once: true})})
+
+const gamesFormModal = document.querySelector(".GameDetailsModal")
+ const submitButton = document.getElementById("submit");
+ const gamePage = document.getElementById("root");
+//  console.log(submitButton);
+
+
+// function preventfalseSubmit(event){
+//     event.preventDefault()
+// }
+
+// gameDetailsForm.addEventListener("submit", preventfalseSubmit);
+
+gameDetailsForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
+
+
+//Display GameBoard
+
+function hideFormDisplayGameBoard(){
+    gamesFormModal.classList.add("GameDetailsModalHidden");
+    gamesFormModal.classList.remove("GameDetailsModal")
+    gamePage.style.backgroundColor = "#541ebf"
+   
+
+}
+
+submitButton.addEventListener("click", hideFormDisplayGameBoard)
+ 
+ 
+//  const cellElements = document.querySelectorAll('.cell');
+// console.log(cellElements);
+
+// const echo = () => console.log("hi");
+// cellElements.forEach(cellElement => {cellElement.addEventListener("click", echo, {once: true})})
