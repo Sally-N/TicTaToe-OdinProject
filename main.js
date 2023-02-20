91958e984932fd2ef98b268e42d4d76de20d003b
Sally-N <snzungula@gmail.com>
2023-02-20 23:01:35 +0300

    handle players

--- main.js
+++ main.js
@@ -15,73 +15,106 @@
 // human vs human, next implement easy ai, next impossible ai
 
 //handle events
- 
- const gameDetailsForm = document.getElementById("formone");
+
+const gameDetailsForm = document.getElementById("formone");
 //  console.log(gameDetailsForm);
 const gameCells = document.getElementsByClassName("cell");
 console.log(gameCells);
 
 
 const gamesFormModal = document.querySelector(".GameDetailsModal")
- const submitButton = document.getElementById("submit");
- const gamePage = document.getElementById("root");
- const gameDisplay = document.querySelector(".game-display")
- console.log(gameDisplay);
+const submitButton = document.getElementById("submit");
+const gamePage = document.getElementById("root");
+const gameDisplay = document.querySelector(".game-display")
+console.log(gameDisplay);
+const restartButton = document.querySelector(".restart");
+const xname = document.getElementById("xname").value;
+const oname = document.getElementById("oname").value;
 
 gameDetailsForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
 })
 
+///global variables
+let player;
+let gameStatus = false;
+let currentPlayer;
+
 
 ///get form data
-function getFormDetails (){
-    const xname = document.getElementById("xname").value;
-    const oname = document.getElementById("oname").value;
+function getFormDetails() {
+    xname;
+    oname;   
     let gameType = '';
 
-    if(document.getElementById("checkboxone").checked){
+    if (document.getElementById("checkboxone").checked) {
         gameType = "humanVshuman"
-    }else if(document.getElementById("checkboxtwo").checked){
+    } else if (document.getElementById("checkboxtwo").checked) {
         gameType = "humanVsEasyAI"
-    }else{
+    } else {
         gameType = "humanVSHardAI"
-    }   
+    }
 
 
-let details = {xname, oname, gameType}
-console.log(details);
+    let details = { xname, oname, gameType }
+    console.log(details);
 
-gamesFormModal.classList.add("GameDetailsModalHidden");
-gamesFormModal.classList.remove("GameDetailsModal")
-gamePage.style.backgroundColor = "#541ebf"
-gameDisplay.style.display = "contents"
+    gamesFormModal.classList.add("GameDetailsModalHidden");
+    gamesFormModal.classList.remove("GameDetailsModal")
+    gamePage.style.backgroundColor = "#541ebf"
+    gameDisplay.style.display = "contents"
 
 
- }
+}
 
 submitButton.addEventListener("click", getFormDetails);
 
- //add event listeners for the each game cell
-const handleClickFactory = (e) => {
-const cell = e.target;
-console.log("hello");
+//add event listeners for the each game cell
+const initiateGameFactory = (e) => {
+    gameStatus = true;
+    console.log("yes", gameStatus);
+
 }
 
- const allCells = Array.from(gameCells)
- allCells.forEach(cell => {
-cell.addEventListener('click', handleClickFactory, { once: true })
-})
+const restartGameFactory = (e) => {
+    gameStatus = false;
+    ///remove all x'x and o's classes and 
+}
+
+const gameBoardCells = () => {
+    const allCells = Array.from(gameCells)
+    allCells.forEach(cell => {
+        cell.addEventListener('click', initiateGameFactory, { once: true });
+        // restartButton.addEventListener('click', restartGame);
+    });
+}
 
 
 
- ////set winning placements
- const winningPlacements = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,4,6],
-    [0,4,8]
+
+const gameMovesFactory = () => {
+    gameBoardCells;
+    initiateGameFactory;
+    getFormDetails;
+
+    const changePlayers = () => {
+        player = (player == xname) ? oname : xname;
+        currentPlayer = (currentPlayer == xname) ? oname : xname;
+        
+    }
+
+}
+
+
+
+////set winning placements
+const winningPlacements = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 6],
+    [0, 4, 8]
 ]
